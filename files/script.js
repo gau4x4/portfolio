55a4de2c61e36aa45ec0cf1ff13a5897a0ad9dc3
Gaurav Pathak <gauravp361@gmail.com>
2023-08-01 20:25:06 -0700

    <Status>

---
+++ files/script.js
@@ -0,0 +1,8 @@
+const say_hello = document.querySelector("#say_hello");
+say_hello.addEventListener("click",()=>{
+    alert("Upcoming Feature! Thanks");
+});
+const briskbite_inc = document.querySelector("#briskbite_inc");
+say_hello.addEventListener("click",()=>{
+    alert("Briskbite got defunct in 2017!");
+});
